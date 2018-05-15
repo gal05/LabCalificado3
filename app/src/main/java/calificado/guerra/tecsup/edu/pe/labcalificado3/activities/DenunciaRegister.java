5fa7db9befe48564a6490aa56bfa43c39fd0ebd4
Guerra Jose <jose.guerra.g@tecsup.edu.pe>
2018-05-15 00:06:47 -0500

    registro denuncias 2

--- app/src/main/java/calificado/guerra/tecsup/edu/pe/labcalificado3/activities/DenunciaRegister.java
+++ app/src/main/java/calificado/guerra/tecsup/edu/pe/labcalificado3/activities/DenunciaRegister.java
@@ -20,10 +20,7 @@ import calificado.guerra.tecsup.edu.pe.labcalificado3.R;
 
 public class DenunciaRegister extends AppCompatActivity {
 
-    private Button captureGPS;
-    private TextView mostrarGPS;
-    private LocationManager locationManager;
-    private LocationListener listener;
+
 
 
     @Override
@@ -31,77 +28,9 @@ public class DenunciaRegister extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_denuncia_register);
 
-        mostrarGPS= (TextView) findViewById(R.id.coordenadas_show);
-        captureGPS = (Button) findViewById(R.id.catchCoordenadas);
 
-        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
-
-
-        listener = new LocationListener() {
-            @Override
-            public void onLocationChanged(Location location) {
-                //mostrarGPS.append("\n " + location.getLongitude() + " " + location.getLatitude());
-                mostrarGPS.setText(location.getLongitude() + " " + location.getLatitude());
-            }
-
-            @Override
-            public void onStatusChanged(String s, int i, Bundle bundle) {
-
-            }
-
-            @Override
-            public void onProviderEnabled(String s) {
-
-            }
-
-            @Override
-            public void onProviderDisabled(String s) {
-
-                Intent i = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
-                startActivity(i);
-            }
-        };
-
-        configure_button();
     }
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        switch (requestCode) {
-            case 10:
-                configure_button();
-                break;
-            default:
-                break;
-        }
-    }
-    void configure_button() {
-        // first check for permissions
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.INTERNET}
-                        , 10);
-            }
-            return;
-        }
-        // this code won't execute IF permissions are not allowed, because in the line above there is return statement.
-        captureGPS.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //noinspection MissingPermission
-                if (ActivityCompat.checkSelfPermission(DenunciaRegister.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(DenunciaRegister.this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                    // TODO: Consider calling
-                    //    ActivityCompat#requestPermissions
-                    // here to request the missing permissions, and then overriding
-                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                    //                                          int[] grantResults)
-                    // to handle the case where the user grants the permission. See the documentation
-                    // for ActivityCompat#requestPermissions for more details.
-                    return;
-                }
-                locationManager.requestLocationUpdates("gps", 5000, 0, listener);
-            }
-        });
-    }
+
 }
 
 
@@ -109,9 +38,3 @@ public class DenunciaRegister extends AppCompatActivity {
 
 
 
-/**
- *https://github.com/miskoajkula/Gps_location.git
- */
-/**
- *https://github.com/modelkit62/GeoAndroid.git----> :v
- */
