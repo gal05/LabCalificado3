49f19e1fd95f16be93672850190c0853a7d333c4
Guerra Jose <jose.guerra.g@tecsup.edu.pe>
2018-05-15 14:42:18 -0500

    login-crearUser-listarDenuncias-CapturarGPS-enviarDenunciaConGPS

--- app/src/main/java/calificado/guerra/tecsup/edu/pe/labcalificado3/activities/DenunciaRegister.java
+++ app/src/main/java/calificado/guerra/tecsup/edu/pe/labcalificado3/activities/DenunciaRegister.java
@@ -5,16 +5,22 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Environment;
 import android.os.StrictMode;
 import android.provider.MediaStore;
+import android.provider.Settings;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
@@ -36,7 +42,7 @@ import retrofit2.Callback;
 
 public class DenunciaRegister extends AppCompatActivity {
 
-
+    private Button gps;
     private ImageView imagenPhoto;
     private EditText titulo;
     private EditText descripcion;
@@ -44,6 +50,11 @@ public class DenunciaRegister extends AppCompatActivity {
     private int usuario_id=Bienvenido.The_codigo;
     private static final String TAG = DenunciaRegister.class.getSimpleName();
 
+    /*
+    *GPS
+     */
+    private LocationManager locationManager;
+    private LocationListener listener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -59,7 +70,37 @@ public class DenunciaRegister extends AppCompatActivity {
         titulo = findViewById(R.id.titulo_input);
         descripcion = findViewById(R.id.descripcion_input);
         coordenadas = findViewById(R.id.coordenadas_input);
+        gps=findViewById(R.id.catchCoordenadas);
+        /*
+        *GPS
+         */
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
 
+
+        listener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                coordenadas.setText("Lon: "+location.getLongitude() + "  Lat: " + location.getLatitude());
+            }
+
+            @Override
+            public void onStatusChanged(String s, int i, Bundle bundle) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String s) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String s) {
+
+                Intent i = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivity(i);
+            }
+        };
+        configure_button();
     }
 
 
@@ -224,7 +265,9 @@ public class DenunciaRegister extends AppCompatActivity {
     private static String[] PERMISSIONS_LIST = new String[]{
             Manifest.permission.CAMERA,
             Manifest.permission.WRITE_EXTERNAL_STORAGE,
-            Manifest.permission.READ_EXTERNAL_STORAGE
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.ACCESS_FINE_LOCATION
     };
 
     private boolean permissionsGranted() {
@@ -271,5 +314,42 @@ public class DenunciaRegister extends AppCompatActivity {
         }
         return Bitmap.createScaledBitmap(bitmap, resizedWidth, resizedHeight, false);
     }
+
+    /*
+     *GPS
+     */
+
+    void configure_button(){
+        // first check for permissions
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.INTERNET}
+                        , 10);
+            }
+            return;
+        }
+        gps.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //noinspection MissingPermission
+                if (ActivityCompat.checkSelfPermission(DenunciaRegister.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(DenunciaRegister.this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    return;
+                }
+                locationManager.requestLocationUpdates("gps", 5000, 0, listener);
+            }
+        });
+    }
+
+
+
+
+
 
 }
